5472f6d725811b251a051cc0899c8a85f1db0361
Shane Liu <shane7226107@gmail.com>
2014-06-11 15:40:16 +0800

    tmp

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,17 +4,19 @@ class ArticlesController < ApplicationController
 	# return an HTML form for creating a new article
 	def new
 		@board_id = params[:board_id]
+		render layout:"user"
 	end
 	# return an HTML form for editing an article
 	def edit
+		render layout:"user"
 	end
 
 	# CRUD actions
 	def create
+		a = Article.new params[:article]
+		a.save!
 
-		# TODO:
-
-		redirect_to :back
+		redirect_to '/backoffice'
 	end	
 	def update
 	end
